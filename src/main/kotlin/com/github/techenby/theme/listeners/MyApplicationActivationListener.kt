d4fdd70529287ee57191241d769bf6004f1e11c6
GitHub Action <action@github.com>
2025-02-16 02:32:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/techenby/theme/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/techenby/theme/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.techenby.theme.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/techenby/theme/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/techenby/theme/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.techenby.theme.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.techenby.theme.MyBundle
+import com.github.techenby.theme.services.MyProjectService
 import javax.swing.JButton
 
 
